91ae580d360d4c6aae51b3a663b21c043ca738cf
donmacdon <don.macadat@gmail.com>
2022-04-21 02:38:14 +0800

    init commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+
+    export async function load({fetch}){
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1`)
+        
+        const data = await res.json()
+
+        if(res.ok){
+            return {
+                props: { popular: data.results }
+            }
+        }
+    }
+</script>
+
+<script>
+    import PopularMovies from "../components/PopularMovies.svelte"
+    export let popular
+</script>
+
+<section>
+    <PopularMovies {popular}/>
+</section>
---
+++ src/routes/movie/[id].svelte
@@ -0,0 +1,22 @@
+<script>
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+
+    let movieid = $page.params.id 
+
+    onMount(async () => {
+		const res = await fetch(`https://api.themoviedb.org/3/movie/${movieid}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`);
+		
+        const data = await res.json();
+
+        console.log(data)
+        if(res.ok){
+            return {
+                props: { movie: data.results }
+            }
+        }
+	});
+
+</script>
+
+
